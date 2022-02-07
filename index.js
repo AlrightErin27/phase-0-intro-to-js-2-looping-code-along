655564eae8b2afa94b942e5fdbe1c057402612fa
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-06 16:41:42 -0800

    completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+//🔥LOOPING LAB🔥//
+
+// const names = [`Guadalupe`, `Ollie`, `Aki`],
+//   event = `surprise`;
+
+//Thank You Cards Function
+function writeCards(names, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(
+      `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankYouCards;
+}
+
+//Count Down Function
+function countDown(startInt) {
+  while (startInt > 0) {
+    console.log(startInt);
+    startInt -= 1;
+  }
+  console.log(startInt);
+}
